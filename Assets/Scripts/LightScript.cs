9845569a11a4633a254bc651f6c49e196490d09b
Danester68 <kjdyson68@gmail.com>
2025-04-21 13:17:04 -0400

    1.2 has been completed!

---
+++ Assets/Scripts/LightScript.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Data.Common;
+using UnityEngine;
+
+public class LightScript : MonoBehaviour
+{
+    public GameObject lightObject1;
+    public GameObject lightObject2;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    
+    public void ToggleLight(String tag) {
+        switch (tag) {
+            case "LightSwitch1":
+                if (lightObject1.activeInHierarchy == true) {
+                    lightObject1.SetActive(false);
+                } else if (lightObject1.activeInHierarchy == false) {
+                    lightObject1.SetActive(true);
+                }
+                break;
+            case "LightSwitch2":
+                if (lightObject2.activeInHierarchy == true) {
+                    lightObject2.SetActive(false);
+                } else if (lightObject2.activeInHierarchy == false) {
+                    lightObject2.SetActive(true);
+                }
+                break;
+            default:
+                Debug.LogError("Tag not correct");
+                break;
+        }
+        
+    }
+}
